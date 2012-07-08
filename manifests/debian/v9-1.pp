ece0854e24ef0abec095e89e7f5bcea79aa7dfac
david <david@david-Latitude-E6320.(none)>
2012-07-08 20:39:18 +0100

    fix to get working under lucid w/ diff apt module

--- manifests/debian/v9-1.pp
+++ manifests/debian/v9-1.pp
@@ -48,6 +48,40 @@ class postgresql::debian::v9-1 {
       }
     }
 
+    lucid: {
+
+      include postgresql::debian::base
+
+      service {"postgresql":
+        ensure    => running,
+        enable    => true,
+        hasstatus => true,
+        start     => "/etc/init.d/postgresql start ${version}",
+        status    => "/etc/init.d/postgresql status ${version}",
+        stop      => "/etc/init.d/postgresql stop ${version}",
+        restart   => "/etc/init.d/postgresql restart ${version}",
+        require   => Package["postgresql-common"],
+      }
+
+      exec { "reload postgresql ${version}":
+        refreshonly => true,
+        command     => "/etc/init.d/postgresql reload ${version}",
+      }
+
+# this uses a different apt module - apt::preference doesn't exist in ours
+/*      apt::preferences {[
+        "libpq5",
+        "postgresql-${version}",
+        "postgresql-client-${version}",
+        "postgresql-common",
+        "postgresql-client-common",
+        "postgresql-contrib-${version}"
+        ]:
+        pin      => "release a=${lsbdistcodename}-backports",
+        priority => "1100",
+      }*/
+    }
+
     default: {
       fail "${name} not available for ${operatingsystem}/${lsbdistcodename}"
     }
