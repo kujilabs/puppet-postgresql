3d169eff3cdd9b296e0c2bb5ea29b88bddfc895b
david <david@david-Latitude-E6320.(none)>
2012-07-12 23:00:50 +0100

    added lucid to case, removed non-working apt::preferences

--- manifests/debian/v9-1.pp
+++ manifests/debian/v9-1.pp
@@ -15,8 +15,7 @@ class postgresql::debian::v9-1 {
   $version = "9.1"
 
   case $lsbdistcodename {
-    squeeze: {
-
+    /^(squeeze|lucid)$/: {
       include postgresql::debian::base
 
       service {"postgresql":
@@ -35,55 +34,23 @@ class postgresql::debian::v9-1 {
         command     => "/etc/init.d/postgresql reload ${version}",
       }
 
-      apt::preferences {[
-        "libpq5",
-        "postgresql-${version}",
-        "postgresql-client-${version}",
-        "postgresql-common",
-        "postgresql-client-common",
-        "postgresql-contrib-${version}"
-        ]:
-        pin      => "release a=${lsbdistcodename}-backports",
-        priority => "1100",
-      }
+# apt::preferences does not work in apt module we're using - class doesn't exist
+#      apt::preferences {[
+#         "libpq5",
+#         "postgresql-${version}",
+#         "postgresql-client-${version}",
+#         "postgresql-common",
+#         "postgresql-client-common",
+#         "postgresql-contrib-${version}"
+#         ]:
+#         pin      => "release a=${lsbdistcodename}-backports",
+#         priority => "1100",
+#       }
+#     }
     }
-
-    lucid: {
-
-      include postgresql::debian::base
-
-      service {"postgresql":
-        ensure    => running,
-        enable    => true,
-        hasstatus => true,
-        start     => "/etc/init.d/postgresql start ${version}",
-        status    => "/etc/init.d/postgresql status ${version}",
-        stop      => "/etc/init.d/postgresql stop ${version}",
-        restart   => "/etc/init.d/postgresql restart ${version}",
-        require   => Package["postgresql-common"],
-      }
-
-      exec { "reload postgresql ${version}":
-        refreshonly => true,
-        command     => "/etc/init.d/postgresql reload ${version}",
-      }
-
-# this uses a different apt module - apt::preference doesn't exist in ours
-/*      apt::preferences {[
-        "libpq5",
-        "postgresql-${version}",
-        "postgresql-client-${version}",
-        "postgresql-common",
-        "postgresql-client-common",
-        "postgresql-contrib-${version}"
-        ]:
-        pin      => "release a=${lsbdistcodename}-backports",
-        priority => "1100",
-      }*/
-    }
-
     default: {
       fail "${name} not available for ${operatingsystem}/${lsbdistcodename}"
-    }
+    }  
   }
+  
 }
